e4268cc8395d6011c761cafa14190fa1a929586f
pownby <pete.ownby@gmail.com>
2025-01-02 03:17:14 -0700

    Yet more task styling

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import cx from 'classnames';
 
 import { Task } from '../../types';
 import Icon from '../Icon';
@@ -12,9 +13,15 @@ type TaskListProps = {
 export default function TaskList({ tasks }: TaskListProps) {
   return !!tasks?.length && (
     <div className={styles.container}>
-      {tasks.map((task) => (
-        <div key={task.id} className={styles.task}>
-          <div className={styles.taskContent}>
+      {tasks.map((task, i) => (
+        <div key={task.id} className={cx(styles.task, styles[task.location])}>
+          {tasks.length > 1 && (
+            <div className={styles.movementContainer}>
+              {i > 0 && <Icon type={Icon.TYPE.ARROW_CIRCLE_UP} onClick={() => console.log(`Move up ${task.name}`)}></Icon>}
+              {i + 1 < tasks.length && <Icon type={Icon.TYPE.ARROW_CIRCLE_DOWN} onClick={() => console.log(`Move down ${task.name}`)}></Icon>}
+            </div>
+          )}
+          <div className={cx(styles.taskContent)}>
             <div className={styles.name}>
               {task.name}
             </div>
@@ -35,7 +42,7 @@ export default function TaskList({ tasks }: TaskListProps) {
               <div>{task.notes}</div>
             </div>}
           </div>
-          <div className={styles.controls}>
+          <div className={styles.controlsContainer}>
             <Icon type={Icon.TYPE.EDIT} onClick={() => console.log(`Edit ${task.name}`)}></Icon>
             <Icon type={Icon.TYPE.COPY} onClick={() => console.log(`Copy ${task.name}`)}></Icon>
             <Icon type={Icon.TYPE.TIMES} onClick={() => console.log(`Remove ${task.name}`)}></Icon>
