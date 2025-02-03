fd938762664f99567d55f654c48f2ef2c8b13d67
pownby <pete.ownby@gmail.com>
2025-02-03 11:56:47 -0700

    Remove extraneous log

--- src/migrations/0.ts
+++ src/migrations/0.ts
@@ -10,7 +10,6 @@ export default function migrate_0_to_1(appData: AppData): AppData {
     return { version: NEXT_VERSION };
   }
 
-  console.log('migrating 0 to 1');
   // normally we'd imagine translating the old schema keys to the new, but since nobody on version 0 has actually used it, we'll just start fresh for simplicity
   return { version: NEXT_VERSION };
 }
